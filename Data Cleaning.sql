a3c78bf971cbc7956edcca6dfb44f142bd9d2b26
Rae12 <43325274+Rae12@users.noreply.github.com>
2024-08-04 12:40:20 -0500

    Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -3,7 +3,9 @@
 --To modify the dataset in R Studio
 
 --Removal of Columns
-TRIPS.csv <- TRIPS.csv[, -c(7, 8, 11, 12)]
+View(Case_Study_TRIPS)
+> Case_Study_TRIPS <- Case_Study_TRIPS [, -c(3,4)]
+> Case_Study_TRIPS <- Case_Study_TRIPS [, -c(9,10,13,14)]
 
 --Save modified dataset in R Studio
 
